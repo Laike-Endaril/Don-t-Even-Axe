51f8071b9a11f13fcbef869ce19f3cadda98b623
Kyle Koder <silvertallon@gmail.com>
2019-05-30 19:47:12 -0700

    Indev build 1.12.2.001a

--- src/main/java/com/fantasticsource/dontevenaxe/DontEvenAxe.java
+++ src/main/java/com/fantasticsource/dontevenaxe/DontEvenAxe.java
@@ -14,7 +14,7 @@ public class DontEvenAxe
 {
     public static final String MODID = "dontevenaxe";
     public static final String NAME = "Don't Even Axe";
-    public static final String VERSION = "1.12.2.001";
+    public static final String VERSION = "1.12.2.001a";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
