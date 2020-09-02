845b09c074fd1465450562a3bba0981075f76377
Kyle Koder <silvertallon@gmail.com>
2020-09-02 02:12:18 -0700

    1.12.2.003

--- src/main/java/com/fantasticsource/dontevenaxe/DontEvenAxe.java
+++ src/main/java/com/fantasticsource/dontevenaxe/DontEvenAxe.java
@@ -17,7 +17,7 @@ public class DontEvenAxe
 {
     public static final String MODID = "dontevenaxe";
     public static final String NAME = "Don't Even Axe";
-    public static final String VERSION = "1.12.2.002";
+    public static final String VERSION = "1.12.2.003";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
